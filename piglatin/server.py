9c5c7e6216b45ef3b51edefc68558fd1b23f8c95
Steve Federowicz <sfederow@gmail.com>
2017-01-04 18:38:43 -0800

    adding simple server and web ui

---
+++ piglatin/server.py
@@ -0,0 +1,39 @@
+""" Simple Flask server for piglatin translator web UI """
+import logging
+from piglatin import translator
+
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+logging.basicConfig(filename='server.log', filemode='w', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@app.route('/translate', methods=['POST'])
+def translate_phrase():
+    """
+    Receive user input from web form and translate to piglatin.
+    """
+    user_input = request.form['text']
+
+    logger.info("User input: %s", user_input)
+
+    translated_input = translator.translate_phrase(user_input)
+
+    logger.info("Translated input: %s", translated_input)
+
+    return render_template('translate_form.html', translation=translated_input, user_input=user_input)
+
+
+@app.route("/")
+def layout():
+    """
+    Layout HTML for main page.
+    """
+    return render_template('translate_form.html', translation='')
+
+
+
+if __name__ == "__main__":
+    app.run()
